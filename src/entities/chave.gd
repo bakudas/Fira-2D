b00aab0d05a14925c36df9fd979333b75119bc93
bakudas <bakudas@gmail.com>
2020-09-07 03:23:13 -0300

    tileset setup, tiles, collisions and sketch a level

--- src/entities/chave.gd
+++ src/entities/chave.gd
@@ -4,15 +4,18 @@ extends Area2D
 signal chave_coletada
 
 
+#destruir a chave ao ser coletada
 func destruir_chave():
 	#$sprite.visible = false
 	queue_free()
 	
 
+# vfx para tocar quando a chave for pega
 func chave_FX():
 	pass
 	
-	
+
+# checa se o jogador colide com a chave
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("jogador"):
 		emit_signal("chave_coletada")
--- src/entities/porta.gd
+++ src/entities/porta.gd
@@ -1,5 +1,5 @@
 extends Sprite
 
-
+# destroi a porta
 func _on_game_manager_abrir_porta_level():
 	$porta.queue_free()
